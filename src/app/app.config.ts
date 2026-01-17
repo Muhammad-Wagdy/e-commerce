b199314e816d8c6dd1d970eabf534f455efd6807
Muhammad-Wagdy <muhammadwagdy00@gmail.com>
2026-01-17 10:50:55 +0200

    less loaders

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -13,7 +13,6 @@ import { provideClientHydration, withEventReplay } from '@angular/platform-brows
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideAngularSvgIcon } from 'angular-svg-icon';
 import { provideToastr } from 'ngx-toastr';
-import { loadingSpinnerInterceptor } from './core/interceptors/loading-spinner-interceptor';
 import { tokenInterceptor } from './core/interceptors/token-interceptor';
 import { errorsInterceptor } from './core/interceptors/errors-interceptor';
 
@@ -24,7 +23,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch(),withInterceptors([loadingSpinnerInterceptor,tokenInterceptor,errorsInterceptor])),
+    provideHttpClient(withFetch(),withInterceptors([tokenInterceptor,errorsInterceptor])),
     provideAnimations(),
     provideAngularSvgIcon(),
     provideToastr({
