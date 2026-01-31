7794dd37c9a9b399331a6a73f2243a290b245add
Muhammad-Wagdy <muhammadwagdy00@gmail.com>
2026-01-31 10:34:09 +0200

    handle auth error

--- src/app/core/interceptors/errors-interceptor.ts
+++ src/app/core/interceptors/errors-interceptor.ts
@@ -2,9 +2,12 @@ import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { catchError, throwError } from 'rxjs';
+import { AuthService } from '../../features/auth/services/auth.service';
 
 export const errorsInterceptor: HttpInterceptorFn = (req, next) => {
   const toastrService = inject(ToastrService)
+  const authService = inject(AuthService)
+  const isLogged = authService.isLoggedIn
 
   return next(req).pipe(
     catchError((error : HttpErrorResponse)=>{
@@ -12,7 +15,7 @@ export const errorsInterceptor: HttpInterceptorFn = (req, next) => {
       if (error.status === 0) {
         toastrService.error('No internet Connection')
       }
-      if (error.status === 401) {
+      if (isLogged && error.status === 401) {
         toastrService.error('UnAuthorized')
       }
 
